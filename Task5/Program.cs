120559ff39a4dcc49e064a6e683bbbd4adace81f
Evgenia Gerasimova <semchenko.jenia@yandex.ru>
2024-02-20 12:58:33 +0300

    добавили 5 каталогов с работами

---
+++ Task5/Program.cs
@@ -0,0 +1,20 @@
+﻿//int a = 10;
+//int b = 15;
+//int c = 3;
+//int d = 4;
+
+//int product4 = a * b;
+//int sum6 = c + d;
+//int quotient3 = product4 / sum6;
+//Console.WriteLine(quotient3); Это домашка
+
+int n = 100;
+int i = 1;
+
+while (i <= n)
+{
+    //Console.WriteLine(i);
+    Console.Write(i);
+    Console.Write (' ');
+    i = i + 1;
+}
